1017bbbba6a1f5049f472d084e8d0a6d5c692be3
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-05 01:27:39 +0800

    added activity page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'activity',
+    loadChildren: () => import('./activity/activity.module').then( m => m.ActivityPageModule)
+  },
 ];
 
 @NgModule({
